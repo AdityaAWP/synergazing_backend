88cb56558b87fa574481c279c2f53e529446614f
AdityaAWP <aditwpratama1@gmail.com>
2025-07-25 14:26:56 +0700

    Basic Library

--- main.go
+++ main.go
@@ -1,7 +1,30 @@
 package main
 
-import "fmt"
+import (
+	"database/sql"
+	"log"
+
+	"github.com/gofiber/fiber/v2"
+	_ "github.com/lib/pq"
+)
 
 func main() {
-	fmt.Println("halo mister")
+	connStr := "postgres://die4wp:root@127.0.0.1:5432/die4wp?sslmode=disable"
+	db, err := sql.Open("postgres", connStr)
+	if err != nil {
+		log.Fatal("Gagal open koneksi:", err)
+	}
+
+	if err := db.Ping(); err != nil {
+		log.Fatal("Gagal koneksi ke database:", err)
+	}
+	log.Println("Database terkoneksi!")
+
+	app := fiber.New()
+
+	app.Get("/", func(c *fiber.Ctx) error {
+		return c.SendString("Hello World")
+	})
+
+	log.Fatal(app.Listen(":3001"))
 }
